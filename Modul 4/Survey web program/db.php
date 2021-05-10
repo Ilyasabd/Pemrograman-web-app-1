2089708d947184c9181a49fa81e4b79afb1ef66e
Ilyasabd <70807037+Ilyasabd@users.noreply.github.com>
2021-05-10 23:23:41 +0700

    Add files via upload

---
+++ Modul 4/Survey web program/db.php
@@ -0,0 +1,10 @@
+<?php
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "";
+	
+	$conn = mysqli_connect($servername, $username, $password, $dbname);
+	$conn->query($query);
+	mysqli_close($conn);
+?>
